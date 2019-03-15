4db67f1fdeaabcd583fc951f88075d026c53b05a
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2019-03-14 21:38:04 -0400

    This is an example of the default constructor
    This is an example of the default constructor

---
+++ Cycle.java
@@ -0,0 +1,23 @@
+public class Cycle {
+
+	
+	//Java Program to create and call a default constructor  
+
+	
+	//creating a default constructor  
+
+	Cycle(){
+
+	System.out.println("Cycle is created");
+
+	}
+
+	//main method  
+
+	public static void main(String args[]){
+		
+		Cycle c = new Cycle();
+	}
+
+	
+}
