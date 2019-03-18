d54c2d7bb429e0c88c3ca71ad5e7333a5edbeab0
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2019-03-17 22:36:14 -0400

    This example of inner class static and non-static example
    This example of inner class static and non-static example

---
+++ Outer_Demo.java
@@ -0,0 +1,20 @@
+public class Outer_Demo {
+
+	int num;
+	   
+	   // inner class
+	   private class Inner_Demo {
+	      public void print() {
+	         System.out.println("This is an inner class");
+	      }
+	   }
+	   
+	   // Accessing he inner class from the method within
+	   void display_Inner() {
+	      Inner_Demo inner = new Inner_Demo();
+	      inner.print();
+	   }
+	}
+	   
+
+ 
